3efb3ade9314a892541f8dfa9313b7971c1deef7
Alex Parkinson <okalex@gmail.com>
2017-10-20 22:29:00 -0700

    Test that  LinkedHashMap insertion order is maintained

--- test/junit/scala/collection/mutable/LinkedHashMapTest.scala
+++ test/junit/scala/collection/mutable/LinkedHashMapTest.scala
@@ -22,4 +22,33 @@ class LinkedHashMapTest {
     lhm.clear()
     Assert.assertNull(lhm.lastItemRef)
   }
+
+  @Test
+  def testMaintainInsertionOrder: Unit = {
+    val lhm = new TestClass
+    Seq("1" -> 1, "a" -> 10, "2" -> 2, "b" -> 9, "3" -> 3, "c" -> 8).foreach(kv => lhm.put(kv._1, kv._2))
+
+    Assert.assertEquals(Set("1", "a", "2", "b", "3", "c"), lhm.keys)
+    Assert.assertEquals(Seq(1, 10, 2, 9, 3, 8), lhm.values.toSeq)
+
+    lhm.remove("a")
+
+    Assert.assertEquals(Set("1", "2", "b", "3", "c"), lhm.keys)
+    Assert.assertEquals(Seq(1, 2, 9, 3, 8), lhm.values.toSeq)
+
+    lhm.remove("3")
+
+    Assert.assertEquals(Set("1", "2", "b", "c"), lhm.keys)
+    Assert.assertEquals(Seq(1, 2, 9, 8), lhm.values.toSeq)
+
+    lhm.remove("1")
+
+    Assert.assertEquals(Set("2", "b", "c"), lhm.keys)
+    Assert.assertEquals(Seq(2, 9, 8), lhm.values.toSeq)
+
+    lhm.remove("c")
+
+    Assert.assertEquals(Set("2", "b"), lhm.keys)
+    Assert.assertEquals(Seq(2, 9), lhm.values.toSeq)
+  }
 }
